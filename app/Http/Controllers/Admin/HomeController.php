fa302cbc2772830abca24ba0716cb4f380c73a4f
Everaldo <torresjr1984@hotmail.com>
2022-09-26 12:54:02 -0300

    Implementação da Dashboard

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -5,16 +5,49 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Visitor;
+use App\Models\Page;
+use App\Models\User;
+
 
 class HomeController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth');
-
     }
 
-    public function index(){
-        return view('admin.home');
+    public function index()
+    {
+
+        $visitsCount = 0;
+        $onlineCount = 0;
+        $pageCount = 0;
+        $userCount = 0;
+
+        //Contagem de visitantes        
+        $visitsCount = Visitor::count();
+
+
+        //Contagem de usúarios online
+        $dateLimit = date('Y-m-d H:i:s', strtotime('-5 minute'));
+        $onlineList = Visitor::select('ip')->where('date_access', '>=', $dateLimit)->groupBy('ip')->get();
+
+        $onlineCount = count($onlineList);
+
+
+        //Contagem de páginas
+        $pageCount = Page::count();
+
+        //Contagem de usuarios
+
+        $userCount = User::count();
+
+        return view('admin.home', [
+            'visitsCount' => $visitsCount,
+            'onlineCount' => $onlineCount,
+            'pageCount' => $pageCount,
+            'userCount' => $userCount
+        ]);
     }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,12 +1,85 @@
 @extends('adminlte::page')
 
-@section('title', 'Painel' )   
+@section('title', 'Painel')
 @section('content_header')
-    <h1>Olá mundo!</h1>
-@endsection 
+    <h1>Dashboard</h1>
+@endsection
 
 
 @section('content')
-    <p>conteúdo</p>
-@endsection
+    <div class="row">
+        <div class="col-md-3">
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h3>{{$visitsCount}}</h3>
+                    <p>Acessos</p>
+                </div>
+                <div class="icon">
+                    <i class="far fa-fw fa-eye">
 
+                    </i>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <h3>{{$onlineCount}}</h3>
+                    <p>Usúarios Online</p>
+                </div>
+                <div class="icon">
+                    <i class="far fa-fw fa-heart">
+
+                    </i>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3>{{$pageCount}}</h3>
+                    <p>Páginas</p>
+                </div>
+                <div class="icon">
+                    <i class="far fa-fw fa-sticky-note">
+
+                    </i>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="small-box bg-danger">
+                <div class="inner">
+                    <h3>{{$userCount}}</h3>
+                    <p>Usuários</p>
+                </div>
+                <div class="icon">
+                    <i class="far fa-fw fa-user"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Páginas mais visitadas</h3>
+                </div>
+                <div class="card-body">
+                    ...
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Sobre o Sistema</h3>
+                </div>
+                <div class="card-body">
+                    ...
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
